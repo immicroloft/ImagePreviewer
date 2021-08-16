9de827a612986bf8ca958ea1390d57364ba503c1
microloft <63073280+immicroloft@users.noreply.github.com>
2021-08-16 08:49:55 +0800

    配合官方更新
    1. 圖片置中
    2. 跳過已可正常顯示的 /i.imgur.com/
    3. 跳過 /imgur.com/a/
    4. 簡化流程

--- imagePreviewer.js
+++ imagePreviewer.js
@@ -1,34 +1,44 @@
 (function() {
 
-// 修改預設樣式，使每張圖片皆獨立佔用一行
+// 在 PTT 網頁版中
+// 每個 <a> 下方皆會自動生成一個 class 為 richcontent 的預覽框
+// 目前 i.imgur.com 的預覽框已可正常顯示
+// 僅剩 imgur.com 的預覽框需要修復
+
+// 修改預設樣式，使每張圖片皆獨立佔用一行並置中
 const style = document.createElement('style')
-style.textContent = 'img.imgur { display: block; }'
+style.textContent = `
+	img.imgur {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+`
 document.head.append(style)
 
-// 用於篩選出包含 imgur.com 的網址，並捕捉網址中的獨特辨識符
-const filter = /imgur\.com\/([^./]+)/
+// 用於篩選出包含 /imgur.com/ 但是並非 /imgur.com/a/ 的網址
+// 並捕捉網址中的獨特辨識符
+const filter = /\/imgur\.com\/(?!a\/)([^./]+)/
 
-// 用於儲存符合篩選條件的元素
-// key 為辨識符，value 為擁有同辨識符的元素陣列
-const map = {}
+// 用於儲存無法正常顯示的預覽框
+const containers = []
 
-// 儲存頁面上符合篩選條件的 <a> 元素
+// 挑出頁面上所有 <a> 元素
 for (const node of document.getElementsByTagName('a')) {
+	// 篩選網址
 	if (!filter.test(node.href))
 		continue
 
-	const key = filter.exec(node.href)[1]
+	// 檢查 <a> 是否屬於自動生成的一部分
+	// 若是，則將整個預覽框加入待移除陣列
+	// 若否，則在下方插入圖片
+	const container = node.closest('.richcontent')
 
-	if (map[key])
-		map[key].push(node)
-	else
-		map[key] = [node]
-}
-
-// 在符合篩選條件的 <a> 元素底下插入對應的 <img> 元素
-for (const [key, nodes] of Object.entries(map))
-	for (const node of nodes) {
+	if (container)
+		containers.push(container)
+	else {
 		const img = document.createElement('img')
+		const key = filter.exec(node.href)[1]
 		img.src = `https://i.imgur.com/${key}.jpg`
 		img.className = 'imgur'
 
@@ -37,16 +47,10 @@ for (const [key, nodes] of Object.entries(map))
 
 		node.after(img)
 	}
+}
 
-// 在 PTT 網頁版中，每個 <a> 底下皆會自動生成一個對應的 <iframe>
-// 儲存並移除這些無法正常顯示，但仍會佔用版面的 <iframe>
-const iframes = []
-
-for (const node of document.getElementsByTagName('iframe'))
-	if (filter.test(node.src))
-		iframes.push(node.parentNode)
-
-for (const node of iframes)
-	node.remove()
+// 移除無法正常顯示的預覽框
+for (const container of containers)
+	container.remove()
 
 })()
